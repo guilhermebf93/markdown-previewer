d9de719b3b873478047ced441b6a9f5c99277d33
Guilherme Ferreira <guilherme.bf93013@gmail.com>
2023-06-11 13:48:50 -0300

    Markdown Previewer First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { marked } from 'marked';
+import text from './text.txt';
+
+marked.use({
+  breaks: true,
+  headerIds: false,
+  mangle: false
+})
+
+function Previewer(props) {
+
+  return (
+    <div id='preview' dangerouslySetInnerHTML={{__html: props.value}} />
+  )
+}
 
 function App() {
+  const [output, setOutput] = useState(null);
+  const [defText, setDefText] = useState(null);
+
+
+  useEffect(() => {
+    fetch(text)
+      .then(response => response.text())
+      .then(text => {
+        const defaultText = text;
+        setDefText(defaultText);
+        if (!output) {
+          const defMarked = marked.parse(defText);
+          setOutput(defMarked);
+        };
+      })
+      .catch(() => setTimeout = 500);    
+  })
+
+  
+  const handleChange = e => {
+    let newInput = marked.parse(e.target.value);
+    setOutput(newInput);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='editor'>
+        <label for='editor'>Editor</label>
+        <textarea id='editor' name='editor' onChange={handleChange} defaultValue={defText} rows={'7'} ></textarea>
+      </div>
+      <div className='preview'>
+        <label for='preview'>Preview</label>
+        <Previewer value={output} />
+      </div>
+    </>
   );
 }
 
